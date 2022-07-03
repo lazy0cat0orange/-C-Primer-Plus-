ed133c0918b62e8a7cfdc81a00048862c0f4fb65
lazy_cat <gjc2001@outlook.com>
2022-07-03 22:29:17 +0800

    3.1 platinum.c

---
+++ 3.1/platinum.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	float weight;//你的体重
+	float value;//相等重量的白金价值
+
+	printf("Are you worth your weight in platinum?\n");
+	printf("Let's check it out.\n");
+	printf("Please enter your weight in pounds:");
+
+	scanf("%f", &weight);
+
+	value = 1700.0 * weight * 14.5833;
+	printf("Your weight in platinum is worth $%.2f.\n", value);
+	printf("You are easily worth that!If platinum prices drop,\n");
+	printf("eat more to maintain your value.\n");
+
+	return 0;
+}
